7e76b4817dab082de3588a36d9a6bef678e4763f
dale <18717394542@163.com>
2014-12-28 16:01:50 +0800

    install lodash and ignore directory of node_modules

--- js/backstage/four-number.js
+++ js/backstage/four-number.js
@@ -3,7 +3,16 @@ function FourNumber() {
 }
 
 FourNumber.formFourNumber = function() {
-  return this.formFourNumber;
+
+  // while(this.fourNumber.length < 4) {
+  //   var formNumber = Math.floor(Math.random() * 10);
+  // }
+  // for(var i = 0; i < 4 ; i++) {
+  //
+  // }
+  // this.formArray.push(Math.floor(Math.random() * 10));
+  // console.log(this.formArray);
+  // return this.formFourNumber;
 };
 
 module.exports = FourNumber;

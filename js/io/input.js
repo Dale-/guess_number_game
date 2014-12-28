b3716f6aef26f74cf4600434266d7e657c3c9ba8
dale <18717394542@163.com>
2014-12-28 22:44:32 +0800

    Coding input.js

--- js/io/input.js
+++ js/io/input.js
@@ -1,9 +1,36 @@
+var readline = require('readline');
+var GameRule = require('../game-rule/game-rule');
+
 function Input() {
   this.inputArray = [];
 }
 
 Input.prototype.consoleInput = function() {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question("Please input four number : \n", function(answer) {
+
+    var array = answer.split('');
+
+    this.inputArray = Input.invertIntType(array);
+
+    console.log(GameRule.getScore([1,2,3,4], this.inputArray));
+
+    //rl.close();
+
+  });
   return this.inputArray;
+};
+
+Input.invertIntType = function(array) {
+
+  for(var i = 0; i < array.length; i++) {
+    array[i] = parseInt(array[i]);
+  }
+  return array;
 };
 
 module.exports = Input;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-function main() {
+var Input = require('./io/input');
 
+main();
+
+function main() {
+  var input = new Input();
+  input.consoleInput();
 }
 
 exports.main = main;

e8d016bd172a8503c59495cde0c0c31d48e002c3
dale <18717394542@163.com>
2014-12-28 19:50:12 +0800

    Coding GameRule.getScore() and test pass

--- __tests__/game-rule/game_rule_test.js
+++ __tests__/game-rule/game_rule_test.js
@@ -6,7 +6,6 @@ describe('GameRule', function() {
   var commonArray;
 
   beforeEach(function() {
-
     GameRule = require('../../js/game-rule/game-rule');
     commonArray = [2,4,6,8];
   });
@@ -15,13 +14,11 @@ describe('GameRule', function() {
   describe('.IsFourNotRepeat', function() {
 
     it('it should be return false of notRepeatNumberArray', function() {
-
       var result = GameRule.IsFourNotRepeat(commonArray);
       expect(result).toBe(false);
     });
 
     it('it should be return false of hasRepeatNumberArray', function() {
-
       var result = GameRule.IsFourNotRepeat([2,6,6,8]);
       expect(result).toBe(true);
     });
@@ -31,19 +28,16 @@ describe('GameRule', function() {
   describe('.getAorBorC', function() {
 
     it('it should be return A', function() {
-
       var result = GameRule.getAorBorC(commonArray, 0, 2);
       expect(result).toBe('A');
     });
 
     it('it should be return B', function() {
-
       var result = GameRule.getAorBorC(commonArray, 1, 2);
       expect(result).toBe('B');
     });
 
     it('it should be return C', function() {
-
       var result = GameRule.getAorBorC(commonArray, 0, 1);
       expect(result).toBe('C');
     });
@@ -52,31 +46,26 @@ describe('GameRule', function() {
   describe('.getScore', function() {
 
     it('it should be return 4A0B', function() {
-
       var result = GameRule.getScore(commonArray, commonArray);
       expect(result).toBe('4A0B');
     });
 
     it('it should be return 0A0B', function() {
-
       var result = GameRule.getScore(commonArray, [0,1,3,9]);
       expect(result).toBe('0A0B');
     });
 
     it('it should be return 2A0B', function() {
-
       var result = GameRule.getScore(commonArray, [0,1,6,8]);
       expect(result).toBe('2A0B');
     });
 
     it('it should be return 0A2B', function() {
-
       var result = GameRule.getScore(commonArray, [6,8,0,1]);
       expect(result).toBe('0A2B');
     });
 
     it('it should be return 1A2B', function() {
-
       var result = GameRule.getScore(commonArray, [2,6,8,0]);
       expect(result).toBe('1A2B');
     });
--- js/game-rule/game-rule.js
+++ js/game-rule/game-rule.js
@@ -5,7 +5,6 @@ function GameRule() {
 
 GameRule.IsFourNotRepeat = function(array) {
   var orderArray = array.sort();
-
   for(var i = 0; i < orderArray.length - 1; i++)
   {
     if (orderArray[i] === orderArray[i+1])
@@ -13,18 +12,35 @@ GameRule.IsFourNotRepeat = function(array) {
       return true;
     }
   }
-
   return false;
 };
 
 GameRule.getAorBorC = function(fourNumber, index, value) {
-
   return _.contains(fourNumber, value) ?
                    (fourNumber.indexOf(value) === index ? 'A' : 'B'): 'C';
 };
 
 GameRule.getScore = function(formArray, inputArray) {
-  return '4A0B';
+
+  var score = { A: 0, B: 0 };
+
+  _.forEach(inputArray, function(number) {
+
+    switch(GameRule.getAorBorC(formArray, inputArray.indexOf(number), number)) {
+      case 'A':
+        score.A ++;
+        break;
+
+      case 'B':
+        score.B ++;
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return score.A + 'A' + score.B + 'B';
 };
 
 

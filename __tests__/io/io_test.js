2b799879732477af26fac4b6d87bd5e96a1a2893
dale <18717394542@163.com>
2014-12-29 20:48:23 +0800

    add beforEach() in io_test.js

--- __tests__/io/io_test.js
+++ __tests__/io/io_test.js
@@ -4,22 +4,25 @@ jest.dontMock('../../js/io/io');
 
 describe('IO', function() {
 
+  var IO;
+  var array;
+  beforeEach(function() {
+    IO = require('../../js/io/io');
+    array = ['2','4','6','8'];
+  });
+
   describe('.invertIntType', function() {
 
     it('it should be return one array that length equal four', function() {
 
-      var IO = require('../../js/io/io');
-      var result = IO.invertIntType(['1','2','3','4']);
-
+      var result = IO.invertIntType(array);
       expect(result.length).toBe(4);
     });
 
 
     it('it should be return EachElementType of array is Number ', function() {
 
-      var IO = require('../../js/io/io');
-      var result = IO.invertIntType(['1','2','3','4']);
-
+      var result = IO.invertIntType(array);
       var isAllTypeNumber = result.every(function(elem) {
         return typeof(elem) === 'number';
       });

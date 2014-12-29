27c603fa22d4cf74d87bed4c95d1331feded3ca2
dale <18717394542@163.com>
2014-12-30 00:36:29 +0800

    add beforEach in four_number_test.js

--- __tests__/backstage/four_number_test.js
+++ __tests__/backstage/four_number_test.js
@@ -3,25 +3,26 @@ jest.dontMock('../../scripts/backstage/four-number');
 
 describe('FourNumber', function() {
 
+  var FourNumber;
+  beforeEach(function() {
+    FourNumber = require('../../scripts/backstage/four-number');
+  });
+
   describe('.formFourNumber', function() {
 
     it('it should be return one array that length equal four', function() {
 
-      var FourNumber = require('../../scripts/backstage/four-number');
       var result = FourNumber.formFourNumber();
-
       expect(result.length).toBe(4);
     });
 
     it('it should be return EachElementType of array is Number ', function() {
 
-      var FourNumber = require('../../scripts/backstage/four-number');
       var result = FourNumber.formFourNumber();
 
       var isAllTypeNumber = result.every(function(elem) {
         return typeof(elem) === 'number';
       });
-
       expect(isAllTypeNumber).toBe(true);
     });
 

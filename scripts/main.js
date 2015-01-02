8ce38c03ae98debb95e765b709651edb2f68d07f
dale <18717394542@163.com>
2015-01-02 20:08:31 +0800

    format main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,5 @@ var FourNumber = require('./backstage/four-number');
 
 
 (function main() {
-  //var io = new IO();
-  //IO.formFourNumber = FourNumber.formFourNumber();
   IO.startGame(0);
 })();
-
-//exports.main = main;
